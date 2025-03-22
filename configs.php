104c8fc9081bee58a32d78188d1048c19f99b909
Fabian-lewis <fabitolewi@gmail.com>
2025-03-22 18:39:15 +0300

    Correct Production Details

--- configs.php
+++ configs.php
@@ -1,12 +1,11 @@
 <?php
-$host = " dpg-cvfadurqf0us73fo3jf0-a.oregon-postgres.render.com";  // Your Render database host
-$dbname = "klms";
+$dsn = " pgsql: host=dpg-cvfadurqf0us73fo3jf0-a.oregon-postgres.render.com;port=5432;dbname=klms;";  // Your Render database host
 $user = "klms_user";
 $password = "CRwSIcxPiQb6sz0k8ShbroeNIrPxhdu0";
 
 try {
-    $pdo = new PDO("pgsql:host=$host;dbname=$dbname", $user, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo = new PDO($dsn, $user, $password, [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]);
+    echo "Database connection successful!";
 } catch (PDOException $e) {
     die("Database connection failed: " . $e->getMessage());
 }
