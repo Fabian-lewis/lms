8fd075fff21866dde2f63078e6d8280a7d4cb5b1
Fabian-lewis <fabitolewi@gmail.com>
2025-04-07 18:43:16 +0300

    STK

--- api/stk_push.php
+++ api/stk_push.php
@@ -1,9 +1,9 @@
 <?php
-session_start();
-if (!isset($_SESSION['user_id'])) {
-    header("Location: login.php");
-    exit();
-}
+// session_start();
+// if (!isset($_SESSION['user_id'])) {
+//     header("Location: login.php");
+//     exit();
+// }
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $phone = $_POST['number']; // User's phone number
